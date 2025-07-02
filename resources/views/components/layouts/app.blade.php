c57d60ad3d053f6221a16d78d6e8d33102f2af62
marco bubola <marcobubola@hotmail.com>
2025-07-02 20:27:22 -0300

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,9 +2,30 @@
     <flux:main>
 
         <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+      
+        <script>
+      window.Promise ||
+        document.write(
+          '<script src="https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js"><\/script>'
+        )
+      window.Promise ||
+        document.write(
+          '<script src="https://cdn.jsdelivr.net/npm/eligrey-classlist-js-polyfill@1.2.20171210/classList.min.js"><\/script>'
+        )
+      window.Promise ||
+        document.write(
+          '<script src="https://cdn.jsdelivr.net/npm/findindex_polyfill_mdn"><\/script>'
+        )
+    </script>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    
+        
+        <link rel="stylesheet" href="{{ asset('assets/css/icon-category.css') }}">
         @livewireStyles
         {{ $slot }}
         @livewireScripts
+        @yield('scripts')
     </flux:main>
 </x-layouts.app.sidebar>
