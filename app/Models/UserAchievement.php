37cb07e437fd78585308a1e32b1581f76b2c5ae6
marco bubola <marcobubola@hotmail.com>
2026-01-17 15:03:28 -0300

    Create UserAchievement.php

---
+++ app/Models/UserAchievement.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserAchievement extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'achievement_id',
+        'unlocked_at',
+        'progress',
+    ];
+
+    protected $casts = [
+        'unlocked_at' => 'datetime',
+        'progress' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function achievement()
+    {
+        return $this->belongsTo(Achievement::class);
+    }
+}
