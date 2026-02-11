ec335934f05e512e87d00d76ba695e3fd55b91ba
marco bubola <marcobubola@hotmail.com>
2026-02-11 20:47:09 -0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Confiar em todos os proxies (ngrok, cloudflare, etc) em desenvolvimento
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
