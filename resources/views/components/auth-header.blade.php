dcd56ea4d12e907eff35bcee74e793a1aae65711
marco bubola <marcobubola@hotmail.com>
2026-04-04 19:27:51 -0300

    Update auth-header.blade.php

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -3,7 +3,7 @@
     'description',
 ])
 
-<div class="flex w-full flex-col text-center">
-    <flux:heading size="xl">{{ $title }}</flux:heading>
-    <flux:subheading>{{ $description }}</flux:subheading>
+<div class="flex w-full flex-col text-center gap-1 mb-2">
+    <h1 class="text-2xl font-bold tracking-tight text-white">{{ $title }}</h1>
+    <p class="text-sm text-zinc-400">{{ $description }}</p>
 </div>
