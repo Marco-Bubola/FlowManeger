979b2737174a37ed2ba34462db8112fc8644880a
marco bubola <marcobubola@hotmail.com>
2026-04-04 18:58:32 -0300

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use App\Traits\HasTeamScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+    use HasTeamScope;
+
+    protected string $teamScopeModule = 'products';
 
     protected $fillable = [
         'name',
