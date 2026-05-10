69b7b6a0131216c8acd0a582f9564afd26d09381
marco bubola <marcobubola@hotmail.com>
2026-05-09 21:37:41 -0300

    Delete drop_shopee2.php

--- drop_shopee2.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require 'bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-DB::statement('DROP TABLE IF EXISTS shopee_orders');
-echo "shopee_orders dropped OK\n";
