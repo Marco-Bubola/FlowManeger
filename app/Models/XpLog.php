8d1c8bed9258f8ed9c50d525a37598bbadb89b16
marco bubola <marcobubola@hotmail.com>
2026-06-07 14:43:58 -0300

    Create XpLog.php

---
+++ app/Models/XpLog.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class XpLog extends Model
+{
+    protected $fillable = [
+        'user_id', 'amount', 'reason', 'source_type', 'source_id',
+    ];
+
+    protected $casts = [
+        'amount' => 'integer',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
